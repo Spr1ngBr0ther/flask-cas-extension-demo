595f3d9fe33476dd430f7543e355a7e53bc0ef3e
Cameron Brandon White <cameronbwhite90@gmail.com>
2014-06-27 19:59:42 -0700

    Made CAS Server changable

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import os
 
 import flask
 from flask import Flask
+from flask import request
 
 import flask_bootstrap
 import flask_cas
@@ -17,14 +18,17 @@ flask_bootstrap.Bootstrap(app)
 cas = flask_cas.CAS(app)
 app.logger.addHandler(logging.StreamHandler())
 app.secret_key = os.environ.get('SECRET_KEY', None)
-app.config['CAS_SERVER'] = os.environ.get('CAS_SERVER', None)
 app.config['CAS_AFTER_LOGIN'] = 'route_root'
 
-@app.route('/')
+@app.route('/',  methods=('GET', 'POST'))
 def route_root():
+    if request.method == 'POST':
+        app.config['CAS_SERVER'] = request.form['cas_server']
+
     return flask.render_template(
         'layout.html',
         username = cas.username,
+        cas_server = app.config.get('CAS_SERVER', None)
     )
 
 def create_parser():
@@ -48,11 +52,6 @@ def create_parser():
        default=False,
     )
     parser.add_argument(
-       "--cas_server", "-c",
-       type=str,
-       required=True,
-    )
-    parser.add_argument(
         "--secret_key", "-S",
         type=str,
         default="DEFAULT SECRET KEY",
@@ -63,7 +62,6 @@ if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
     app.secret_key = args.secret_key
-    app.config['CAS_SERVER'] = args.cas_server
     app.run(
         host=args.host, 
         port=args.port,
